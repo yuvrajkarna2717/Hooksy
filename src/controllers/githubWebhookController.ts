ba445cc69e4bb9be839919068853ff55b1a5747b
Yuvraj Karna <yuvraj.karna@iquadra.com>
2025-06-06 17:12:52 +0530

    fix: added log

--- src/controllers/githubWebhookController.ts
+++ src/controllers/githubWebhookController.ts
@@ -3,11 +3,18 @@ import { WebhookService } from "../services/webhookService";
 
 const webhookService = new WebhookService();
 export const githubWebHook = async (req: Request, res: Response) => {
+  console.log("req")
   const eventType = req.header("X-GitHub-Event") || "";
-  if (!eventType)  res.status(400).send("Missing GitHub Event Type");
+  if (!eventType)  {
+    res.status(400).send("Missing GitHub Event Type");
+    return;
+  }
+
+  console.log("eventType", eventType);
 
   try {
     await webhookService.handleWebhook(req.body, eventType);
+    console.log("webhook called");
     res.status(200).send("Webhook handled");
   } catch (err) {
     console.error("Webhook error:", err);
